3f8593ef05c75530774d24a4a96e5e20ee05095c
mahfuz <mdmahfuzbd@gmail.com>
2021-03-05 01:35:11 +0600

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Button, Jumbotron } from 'react-bootstrap';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Jumbotron>
+        <h1> NewsApi</h1>
+      </Jumbotron>
+     <Button>Hello</Button>
+      
     </div>
   );
 }
